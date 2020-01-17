d95c7796a4e748410013bb145503879616992061
Sethuraman Krishnamoorthy <ramansethu605@gmail.com>
2020-01-17 10:49:36 +0530

    Vanilla GraphQL implementation in without Express/Apollo

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
-var express = require('express');
-var graphqlHTTP = require('express-graphql');
-var { buildSchema } = require('graphql');
+var { graphql, buildSchema } = require('graphql');
 
+// Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
   type Query {
     hello: String
   }
 `);
 
-var root = { hello: () => 'Hello world!' };
+// The root provides a resolver function for each API endpoint
+var root = {
+  hello: () => {
+    return 'Hello world!';
+  },
+};
 
-var app = express();
-
-app.use('/graphql', graphqlHTTP({
-    schema: schema,
-    rootValue: root,
-    graphiql: true,
-}));
-
-app.listen(4000, () => console.log('Now browse to localhost:4000/graphql'));
+// Run the GraphQL query '{ hello }' and print out the response
+graphql(schema, 'query { hello }', root).then((response) => {
+  console.log(JSON.stringify(response));
+});
