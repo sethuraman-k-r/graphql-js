ec48be3199301f258dbfc79605d01ee29dabe828
Sethuraman Krishnamoorthy <ramansethu605@gmail.com>
2020-01-17 12:23:40 +0530

    Simple mutation

--- index.js
+++ index.js
@@ -5,38 +5,23 @@ const { buildSchema } = require('graphql');
 const port = 4000;
 
 var schema = buildSchema(`
-    type RandomDie {
-        rollOnce: Int!
-        numSides: Int!
-        roll(numRolls: Int!): [Int]
+    type Mutation {
+        setMessage(message: String): String
     }
     
     type Query {
-        getDie(numSides: Int): RandomDie
+        getMessage: String
     }
 `);
 
-class RandomDie {
-    constructor(numSides) {
-        this.numSides = numSides;
-    }
-
-    rollOnce() {
-        return 1 + Math.floor(Math.random() * this.numSides);
-    }
-
-    roll({ numRolls }) {
-        var output = [];
-        for (var i = 0; i < numRolls; i++) {
-            output.push(this.rollOnce());
-        }
-        return output;
-    }
-}
-
+var fakeDatabase = {};
 var root = {
-    getDie: ({ numSides }) => {
-        return new RandomDie(numSides || 6);
+    setMessage: ({ message }) => {
+        fakeDatabase.message = message;
+        return message;
+    },
+    getMessage: () => {
+        return fakeDatabase.message;
     }
 };
 
