d8c1ac0affc7b3017437ad7fa546633a7aff0932
Sethuraman Krishnamoorthy <ramansethu605@gmail.com>
2020-01-17 11:14:29 +0530

    Passing arguments

--- index.js
+++ index.js
@@ -6,23 +6,19 @@ const port = 4000;
 
 var schema = buildSchema(`
     type Query {
-        quoteOfTheDay: String
-        random: Float!
-        rollThreeDice: [Int]
+        rollDice(numDice: Int!, numSides: Int): [Int]
     }
 `);
 
 var root = {
-    quoteOfTheDay: () => {
-      return Math.random() < 0.5 ? 'Take it easy' : 'Salvation lies within';
-    },
-    random: () => {
-      return Math.random();
-    },
-    rollThreeDice: () => {
-      return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
-    },
-  };
+    rollDice: (args) => {
+        var output = [];
+        for (var i = 0; i < args.numDice; i++) {
+            output.push(1 + Math.floor(Math.random() * (args.numSides || 6)));
+        }
+        return output;
+    }
+};
 
 const app = express();
 
