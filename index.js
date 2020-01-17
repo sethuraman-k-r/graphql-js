dad6bef9cf1770cd56fd21688daea9aafbce2d3d
Sethuraman Krishnamoorthy <ramansethu605@gmail.com>
2020-01-17 11:01:44 +0530

    GraphQL implementation in JS using Express Framework

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
-var { graphql, buildSchema } = require('graphql');
+const express = require('express');
+const graphqlHTTP = require('express-graphql');
+const { buildSchema } = require('graphql');
 
-// Construct a schema, using GraphQL schema language
+const port = 4000;
+
 var schema = buildSchema(`
-  type Query {
-    hello: String
-  }
+    type Query {
+        hello: String
+    }
 `);
 
-// The root provides a resolver function for each API endpoint
 var root = {
-  hello: () => {
-    return 'Hello world!';
-  },
+    hello: () => {
+        return 'Hello World!!!';
+    },
 };
 
-// Run the GraphQL query '{ hello }' and print out the response
-graphql(schema, 'query { hello }', root).then((response) => {
-  console.log(JSON.stringify(response));
+const app = express();
+
+app.use('/graphql', graphqlHTTP({
+    schema: schema,
+    rootValue: root,
+    graphiql: true
+}));
+
+app.listen(port, () => {
+    console.log(`Running a GraphQL API server at http://localhost:${port}/graphql`);
 });
