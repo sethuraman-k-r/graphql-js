0ccb6c225489bfcacccbeb4440c390a2183d03eb
Sethuraman Krishnamoorthy <ramansethu605@gmail.com>
2020-01-17 12:32:01 +0530

    Mutation with input type for reuse

--- index.js
+++ index.js
@@ -5,24 +5,62 @@ const { buildSchema } = require('graphql');
 const port = 4000;
 
 var schema = buildSchema(`
-    type Mutation {
-        setMessage(message: String): String
+    input MessageInput {
+        content: String
+        author: String
+    }
+    
+    type Message {
+        id: ID!
+        content: String
+        author: String
     }
     
     type Query {
-        getMessage: String
+        getMessage(id: ID!): Message
+    }
+    
+    type Mutation {
+        createMessage(input: MessageInput): Message
+        updateMessage(id: ID!, input: MessageInput): Message
     }
 `);
 
-var fakeDatabase = {};
-var root = {
-    setMessage: ({ message }) => {
-        fakeDatabase.message = message;
-        return message;
-    },
-    getMessage: () => {
-        return fakeDatabase.message;
+
+// If Message had any complex fields, we'd put them on this object.
+class Message {
+    constructor(id, { content, author }) {
+        this.id = id;
+        this.content = content;
+        this.author = author;
     }
+}
+
+// Maps username to content
+var fakeDatabase = {};
+
+var root = {
+    getMessage: ({ id }) => {
+        if (!fakeDatabase[id]) {
+            throw new Error('no message exists with id ' + id);
+        }
+        return new Message(id, fakeDatabase[id]);
+    },
+    createMessage: ({ input }) => {
+        // Create a random id for our "database".
+        var id = new Date().getTime();
+        console.log(input)
+        fakeDatabase[id] = input;
+        return new Message(id, input);
+    },
+    updateMessage: ({ id, input }) => {
+        if (!fakeDatabase[id]) {
+            throw new Error('no message exists with id ' + id);
+        }
+        // This replaces all old data, but some apps might want partial update.
+        fakeDatabase[id] = input;
+        return new Message(id, input);
+    },
 };
 
 const app = express();
